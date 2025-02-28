b39de7ff5c48c49635836428b741d4e0565e6bf6
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-02-28 22:12:43 +0500

    добавлена функция create_object_from_json в src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,9 @@
 import json
 import os
 
+from src.category import Category
+from src.product import Product
+from typing import Any
 
 def read_json(path: str) -> dict:
     """Считывает данные из файла JSON и возвращает словарь
@@ -11,7 +14,16 @@ def read_json(path: str) -> dict:
     return data
 
 
+def create_object_from_json(data: Any) -> list:
+    """Получает данные из словаря, преобразовывает их в класс Category
+    и возвращает список данных
+    """
+    categories = []
+    for category in data:
+        goods = []
+        for good in category['products']:
+            goods.append(Product(**good))
+        category['products'] = goods
+        categories.append(Category(**category))
 
-
-
-
+    return categories
