5354af216ac8001036d32982891ecc33a784bbd6
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-03-02 00:39:12 +0500

    add tests/test_category.py

---
+++ tests/test_category.py
@@ -0,0 +1,13 @@
+from src.category import Category
+
+
+def test_category_init(first_category: Category, second_category: Category) -> None:
+    assert first_category.name == "Smart"
+    assert first_category.description == "Смартфоны, как средство не только коммуникации"
+    assert len(first_category.products) == 2
+
+    assert first_category.category_count == 2
+    assert second_category.category_count == 2
+
+    assert first_category.product_count == 3
+    assert second_category.product_count == 3
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -10,18 +10,6 @@ def test_product_init(product: Product) -> None:
     assert product.description == "256GB, Серый цвет, 200MP камера"
 
 
-def test_category_init(first_category: Category, second_category: Category) -> None:
-    assert first_category.name == "Smart"
-    assert first_category.description == "Смартфоны, как средство не только коммуникации"
-    assert len(first_category.products) == 2
-
-    assert first_category.category_count == 2
-    assert second_category.category_count == 2
-
-    assert first_category.product_count == 3
-    assert second_category.product_count == 3
-
-
 def test_read_json() -> None:
 
     # Создаем тестовый JSON-файл
