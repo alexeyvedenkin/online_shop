2d02adc56c48d0488efd19e8bedbfed87bde8e77
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-03-04 00:46:04 +0500

    добавлено интерактивное подтверждение при уменьшении цены продукта

--- src/product.py
+++ src/product.py
@@ -30,15 +30,20 @@ class Product:
 
     @price.setter
     def price(self, new_price: float):
+        price_reducing_confirmation = True
         if new_price <= 0:
             print('Цена не должна быть нулевая или отрицательная')
             return
-        self.__price = new_price
-
+        elif new_price < self.__price:
+            if input(f'Старая цена: {self.__price}руб., новая цена: {new_price}руб. Подтверждаете уменьшение цены? y/n : ') not in ['Y', 'y']:
+                price_reducing_confirmation = False
+        if price_reducing_confirmation:
+            self.__price = new_price
+        return self.__price
 
 #
 if __name__ == '__main__':
-    product2 = Product("Honor 70S+ pro", "512GB, Черный цвет, 400MP камера", 180000.0, 5)
+    product2 = Product("Honor 70 Pro+", "512GB, Черный цвет, 400MP камера", 180000.0, 5)
     print(product2.name)
     print(product2.description)
     print(product2.price)
@@ -46,5 +51,7 @@ if __name__ == '__main__':
 
     product2.price = -10000.0
     print(product2.price)
-    product2.price = 210000.0
+    product2.price = 110000.0
+    print(product2.price)
+    product2.price = 120000.0
     print(product2.price)
