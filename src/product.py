71a7234cb394059e2dab920e95d2426cd4cc6afd
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-03-09 17:36:18 +0500

    реализованы методы __str__ и __add__ в классе Product

--- src/product.py
+++ src/product.py
@@ -18,6 +18,12 @@ class Product:
         # Добавление продукта в список all_products
         Product.all_products.append(self)
 
+    def __str__(self) -> str:
+        return f"{self.name}, {self.price} руб. Остаток: {self.quantity} шт.\n"
+
+    def __add__(self, other):
+        return self.__price * self.quantity + other.__price * other.quantity
+
     @property
     def price(self) -> Any:
         return self.__price
@@ -55,3 +61,23 @@ class Product:
             )
             cls.all_products.append(new_product)  # Добавление в список
         return new_product
+
+if __name__ == '__main__':
+    product1 = Product("Samsung Galaxy S23 Ultra", "256GB, Серый цвет, 200MP камера", 180000.0, 5)
+    product2 = Product("Iphone 15", "512GB, Gray space", 210000.0, 8)
+    product3 = Product("Xiaomi Redmi Note 11", "1024GB, Синий", 31000.0, 14)
+
+    print(product1)
+    print(product2)
+    print(product3)
+
+    print(product1 + product3)
+
+    # category1 = Category(
+    #     "Смартфоны",
+    #     "Смартфоны, как средство не только коммуникации, но и получения дополнительных функций для удобства жизни",
+    #     [product1, product2, product3]
+    # )
+    # print(category1.products_in_list)
+    #
+    # print(category1)
