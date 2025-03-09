e2e934fc39b7220c1958e4e53a8df10498cce4fd
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-03-10 01:05:44 +0500

    настройка тестов для итератора

--- tests/test_category.py
+++ tests/test_category.py
@@ -1,3 +1,5 @@
+import pytest
+
 from src.category import Category
 from src.product import Product
 
@@ -10,9 +12,6 @@ def test_category_init(first_category: Category, second_category: Category) -> N
     assert first_category.category_count == 2
     assert second_category.category_count == 2
 
-    # assert first_category.product_name_count == 3
-    # assert second_category.product_name_count == 3
-
 
 def test_category_products_property(first_category: Category) -> None:
     assert first_category.products == ("Samsung Galaxy S23 Ultra, 180000.0 руб. Остаток: 2 шт.\n"
@@ -23,3 +22,16 @@ def test_category_products_setter(first_category: Category, product: Product) ->
     assert len(first_category.products_in_list) == 2
     first_category.products = product
     assert len(first_category.products_in_list) == 3
+
+
+def test_category_str(first_category)-> None:
+    assert str(first_category) == "Smart, количество продуктов: 7 шт."
+
+
+def test_product_iterator(product_iterator) -> None:
+    assert product_iterator.index == 0
+    assert next(product_iterator).name == "Samsung Galaxy S23 Ultra"
+    assert next(product_iterator).name == "Samsung Galaxy S24 Ultra"
+    with pytest.raises(StopIteration):
+        next(product_iterator)
+
