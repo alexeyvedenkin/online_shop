8a689fe12ac88f14e4057ec83b2583c34239cf33
alexey_vedenkin <alexey.vedenkin@mail.ru>
2025-03-22 20:18:13 +0500

    переопределены тесты для класса Order

--- tests/test_order.py
+++ tests/test_order.py
@@ -1,23 +1,25 @@
+import pytest
+
 from src.order import Order
 
 
-def test_order_init(product, quantity=2) -> None:
-    order = Order(product, 2)
-    assert order.id_num == 1
+def test_order_init() -> None:
+    order = Order()
+    assert order.product is None
+    assert order.quantity == 0
+
+
+def test_add_product_success(product):
+    order = Order()
+    order.add_product(product, 2)
     assert order.product == product
     assert order.quantity == 2
-
-
-def test_order_add_product(test_product4, quantity=2) -> None:
-    order = Order(test_product4, 2)
-    product2 = test_product4
-    new_order = order.add_product(product2, 3)
-    assert new_order.product == test_product4
-    assert new_order.quantity == 3
+    assert order.calculate_total_cost() == 360000.0
 
 
 def test_order_str(test_product6):
-    order = Order(test_product6, 1)
+    order = Order()
+    order.add_product(test_product6, 1)
     order_str = str(order)
     assert order_str.startswith("Заказ №")
     assert test_product6.name in order_str
